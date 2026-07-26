6009900dce500ec4399aa35a860fd1957c2fd219
sangel3232 <sangel-2022a@corhuila.edu.co>
2026-07-25 19:48:21 -0500

    Corrige contenido del sitemap

--- src/views/Hallway/Hallway.tsx
+++ src/views/Hallway/Hallway.tsx
@@ -46,20 +46,20 @@ const DOORS: DoorConfig[] = [
   {
     slug: 'ceo', label: 'Oficina del CEO', sublabel: 'Emmanuel Segura Zapata',
     doorImage: '/images/puertas-oficinadelCEO-bg.jpg',
-    hotspotLeft: '94%', hotspotTop: '22%', hotspotLeftMobile: '97%', hotspotTopMobile: '32%', side: 'right',
+    hotspotLeft: '93%', hotspotTop: '22%', hotspotLeftMobile: '96%', hotspotTopMobile: '32%', side: 'right',
     route: '/ceo',
     color: '#E8C96A',
   },
   {
     id: 6, slug: 'derechos-autor', label: 'Derechos de Autor', sublabel: 'Departamento 6',
     doorImage: '/images/puerta-derechosdeautor-bg.jpg',
-    hotspotLeft: '83%', hotspotTop: '33%', hotspotLeftMobile: '87%', hotspotTopMobile: '36%', side: 'right',
+    hotspotLeft: '82%', hotspotTop: '33%', hotspotLeftMobile: '86%', hotspotTopMobile: '36%', side: 'right',
     route: '/department/derechos-autor',
   },
   {
     id: 4, slug: 'marketing-lanzamientos', label: 'Marketing y Lanzamientos', sublabel: 'Departamento 4',
     doorImage: '/images/puerta-marketingylanzamientos-bg.jpg',
-    hotspotLeft: '73%', hotspotTop: '44%', hotspotLeftMobile: '79%', hotspotTopMobile: '40%', side: 'right',
+    hotspotLeft: '72%', hotspotTop: '44%', hotspotLeftMobile: '78%', hotspotTopMobile: '40%', side: 'right',
     route: '/department/marketing-lanzamientos',
   },
 ]
